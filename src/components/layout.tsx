ab4a1cc8d20f1e51d2e98c8f353912c9101dc392
ytakhs <21127627+ytakhs@users.noreply.github.com>
2022-03-06 20:30:13 +0900

    Modify header style

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -10,13 +10,13 @@ const pcWidth = 'm-auto lg:max-w-3xl lg:w-full w-10/12';
 
 export function Layout({ children }: Props) {
   return (
-    <div className="flex flex-col flex-1">
+    <div className="flex flex-col">
       <Head>
         <title>ytakhs.com</title>
       </Head>
 
       <header className="sticky top-0 py-4 bg-white dark:bg-neutral-800 shadow">
-        <div className={`${pcWidth} px-4`}>
+        <div className={`${pcWidth}`}>
           <Link href="/">
             <a>ytakhs.com</a>
           </Link>
@@ -26,6 +26,7 @@ export function Layout({ children }: Props) {
       <main className="p-8 m-auto my-4 w-11/12 min-h-screen bg-white dark:bg-neutral-800 rounded-md shadow-md lg:w-full lg:max-w-4xl">
         <div className={`${pcWidth} py-4 w-full`}>{children}</div>
       </main>
+
       <footer className="p-8"></footer>
     </div>
   );
