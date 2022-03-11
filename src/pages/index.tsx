d07b6932a5ee3180eb51b0cbc616d6f9d6df7540
ytakhs <21127627+ytakhs@users.noreply.github.com>
2022-03-11 23:16:48 +0900

    Modify top style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,28 +17,26 @@ const Home: NextPage = () => {
 
       <Layout>
         <h1 className="hidden">ytakhs.com</h1>
-        <div className="my-4">
-          <ul className="px-2">
-            <li>
-              <Link href="/entries">
-                <a>
-                  <div className="py-4 border-b">
-                    Blog <ChevronRightIcon className="inline w-5 h-5" />
-                  </div>
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="https://github.com/ytakhs">
-                <a target="_blank">
-                  <div className="py-4 border-b">
-                    GitHub <ExternalLinkIcon className="inline w-5 h-5" />
-                  </div>
-                </a>
-              </Link>
-            </li>
-          </ul>
-        </div>
+        <ul>
+          <li>
+            <Link href="/entries">
+              <a>
+                <div className="py-4 border-b">
+                  Blog <ChevronRightIcon className="inline w-5 h-5" />
+                </div>
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="https://github.com/ytakhs">
+              <a target="_blank">
+                <div className="py-4 border-b">
+                  GitHub <ExternalLinkIcon className="inline w-5 h-5" />
+                </div>
+              </a>
+            </Link>
+          </li>
+        </ul>
       </Layout>
     </>
   );
