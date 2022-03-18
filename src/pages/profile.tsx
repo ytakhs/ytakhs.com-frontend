5eec7a20528e19715aac7395c0a55a37e7decf50
ytakhs <21127627+ytakhs@users.noreply.github.com>
2022-03-18 22:08:03 +0900

    Add profile page

---
+++ src/pages/profile.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BreadcrumbItem } from '../components/breadcrumb';
+import { Layout } from '../components/layout';
+import { Markdown } from '../components/markdown';
+
+const rawMd = `
+`;
+
+export default function Profile() {
+  return (
+    <>
+      <Layout breadcrumb={<BreadcrumbItem href="/profile" text="profile" />}>
+        <h1>Profile</h1>
+        <article>
+          <Markdown rawMd={rawMd} />
+        </article>
+      </Layout>
+    </>
+  );
+}
