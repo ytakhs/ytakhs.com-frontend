afae14e6b04bfea7d0a1b7aa1d1d4581b305cf2a
ytakhs <21127627+ytakhs@users.noreply.github.com>
2022-04-18 22:09:38 +0900

    Add og:image

--- src/components/og.tsx
+++ src/components/og.tsx
@@ -9,12 +9,15 @@ type OgProps = {
 };
 
 export function Og({ title, description, entryPath, ogType }: OgProps) {
+  const baseURL = 'https://ytakhs.com';
+
   return (
     <Head>
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:type" content={ogType} />
-      <meta property="og:url" content={`https://ytakhs.com${entryPath}/`} />
+      <meta property="og:url" content={`${baseURL}${entryPath}/`} />
+      <meta property="og:image" content={`${baseURL}/images/og-image.png`} />
       <meta property="twitter:card" content="summary" />
     </Head>
   );
